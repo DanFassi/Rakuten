2f7e20f16e47c2d57c7aad4b78b51c2360960057
DanFassi <114923732+DanFassi@users.noreply.github.com>
2023-05-31 01:14:55 +0200

    Update test_api2.py

--- test_api2.py
+++ test_api2.py
@@ -1,67 +1,67 @@
 import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from fastapi import FastAPI
 from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
 from api_rak import app  
 
 @pytest.fixture
 def client():
-    with AsyncClient(app=app, base_url="http://localhost:8000") as client:
+    with TestClient(app) as client:
         yield client
 
 
-async def test_create_user(client):
+def test_create_user(client):
     # Prepare test data
     form_data = {"username": "testuser", "password": "testpass", "role" : "user"}
 
     # Send a POST request to create a new user
-    response = await client.post("/user/creation", data=form_data)
+    response = client.post("/user/creation", data=form_data)
 
     # Verify the response
     assert response.status_code == HTTP_201_CREATED
     assert "Nouvel utilisateur créée" in response.text
 
-async def test_predict(client):
+def test_predict(client):
     # Prepare test data
     form_data = {"designation": "Test Designation", "description": "Test Description"}
 
     # Send a POST request to make a prediction
-    response = await client.post("/predict", data=form_data)
+    response = client.post("/predict", data=form_data)
 
     # Verify the response
     assert response.status_code == 200
 
-async def test_delete_user(client):
+def test_delete_user(client):
     # Prepare test data
     username = "testuser"
 
     # Send a DELETE request to delete a user
-    response = await client.delete("/admin/{}".format(username))
+    response =  client.delete("/admin/{}".format(username))
 
     # Verify the response
     assert response.status_code == HTTP_204_NO_CONTENT
 
-async def test_update_user(client):
+def test_update_user(client):
     # Prepare test data
     username = "testuser"
-    user_data = {"role": "admin"}
+    user_data = {"role": "user"}
 
     # Send a PUT request to update a user
-    response = await client.put("/admin/{}".format(username), json=user_data)
+    response = client.put("/admin/{}".format(username), json=user_data)
 
     # Verify the response
     assert response.status_code == 200
     assert response.json()["name"] == username
-    assert response.json()["role"] == "admin"
+    assert response.json()["role"] == "user"
 
 
-async def test_update_user_not_found(client):
+def test_update_user_not_found(client):
     # Prepare test data
     username = "nonexistentuser"
-    user_data = {"role": "admin"}
+    user_data = {"role": "user"}
 
     # Send a PUT request to update a non-existent user
-    response = await client.put(f"/{username}", json=user_data)
+    response = client.put("/admin/{}".format(username), json=user_data)
 
     assert response.status_code == HTTP_404_NOT_FOUND
     assert "User with name" in response.text

2da6f00dd4672e36dec700aed85c0103da838482
DanFassi <114923732+DanFassi@users.noreply.github.com>
2023-05-31 10:01:48 +0200

    Update test_api2.py

--- test_api2.py
+++ test_api2.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
-from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND, HTTP_200_OK
 from api_rak import app  
 
 @pytest.fixture
@@ -9,59 +9,43 @@ def client():
     with TestClient(app) as client:
         yield client
 
-
+#Requete testant la création d'un utilisateur dans la base
 def test_create_user(client):
-    # Prepare test data
     form_data = {"username": "testuser", "password": "testpass", "role" : "user"}
-
-    # Send a POST request to create a new user
     response = client.post("/user/creation", data=form_data)
-
-    # Verify the response
-    assert response.status_code == HTTP_201_CREATED
+    assert response.status_code == HTTP_200_OK
     assert "Nouvel utilisateur créée" in response.text
 
+
 def test_predict(client):
-    # Prepare test data
     form_data = {"designation": "Test Designation", "description": "Test Description"}
-
-    # Send a POST request to make a prediction
     response = client.post("/predict", data=form_data)
-
-    # Verify the response
     assert response.status_code == 200
 
-def test_delete_user(client):
-    # Prepare test data
-    username = "testuser"
 
-    # Send a DELETE request to delete a user
-    response =  client.delete("/admin/{}".format(username))
-
-    # Verify the response
-    assert response.status_code == HTTP_204_NO_CONTENT
-
+#Requete testant la maj'un utilisateur dans la base
 def test_update_user(client):
-    # Prepare test data
     username = "testuser"
-    user_data = {"role": "user"}
-
-    # Send a PUT request to update a user
+    password = "changement"
+    user_data = {"name":username, "password" : password, "role": "user"}
     response = client.put("/admin/{}".format(username), json=user_data)
-
-    # Verify the response
     assert response.status_code == 200
     assert response.json()["name"] == username
+    assert response.json()["role"] == "password"
     assert response.json()["role"] == "user"
 
+#Requete supprimant un utilisateur de la base
+def test_delete_user(client):
+    username = "testuser"
+    response =  client.delete("/admin/{}".format(username))
+    assert response.status_code == HTTP_204_NO_CONTENT
 
+#Requete testant la réponse si l'on tente de maj un utilisateur qui n'existe pas dans la base
 def test_update_user_not_found(client):
-    # Prepare test data
     username = "nonexistentuser"
+    password = "changement"
+    user_data = {"name":username, "password" : password, "role": "user"}
     user_data = {"role": "user"}
-
-    # Send a PUT request to update a non-existent user
     response = client.put("/admin/{}".format(username), json=user_data)
-
     assert response.status_code == HTTP_404_NOT_FOUND
     assert "User with name" in response.text
